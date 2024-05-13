6120411f90826398d9f68dcc54e0775fa94c6ade
AEntropia <sandman2871@gmail.com>
2024-05-12 22:08:28 -0300

    JWT implemtentado

---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import {config} from 'dotenv'
+config() // carrega as variáveis do .env
+
+const app = express()
+const {PORT} = process.env
+//Import das rotas da aplicação
+import RotasBeneficio from './routes/beneficio.js'
+import RotasUsuarios from './routes/usuario.js'
+app.use(express.json()) //Habilita o parse do JSON
+//Rota de conteúdo público
+app.use(express.static('public'))
+//Removendo o x-powered-by por segurança
+app.disable('x-powered-by')
+//Configurando o favicon
+app.use('/favicon.ico', express.static('public/images/logo-api.png'))
+
+//Rota default
+app.get('/api', (req, res)=> {
+    res.status(200).json({
+        message: 'API FATEC 100% funcional🚀',
+        version: '1.0.0'
+    })
+})
+//Rotas da API
+app.use('/api/beneficio', RotasBeneficio)
+app.use('/api/usuario', RotasUsuarios)
+//Listen
+app.listen(PORT, function(){
+    console.log(`💻Servidor rodando na porta ${PORT}`)
+})
